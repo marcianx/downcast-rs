26927b6bcb73cef1050dde97ffc0e04ecf02b418
Ashish Myles <marcianx@gmail.com>
2025-01-05 00:08:17 -0500

    Specify edition and suppress dead-code warnings in compilation tests.

--- tests/import_via_macro_use.rs
+++ tests/import_via_macro_use.rs
@@ -4,6 +4,7 @@ extern crate downcast_rs;
 #[test]
 fn test() {
     use downcast_rs::Downcast;
+    #[allow(dead_code)]
     trait Trait: Downcast {}
     impl_downcast!(Trait);
 }
--- tests/use_via_namespace.rs
+++ tests/use_via_namespace.rs
@@ -3,6 +3,7 @@ extern crate downcast_rs;
 #[test]
 fn test() {
     use downcast_rs::Downcast;
+    #[allow(dead_code)]
     trait Trait: Downcast {}
     downcast_rs::impl_downcast!(Trait);
 }
